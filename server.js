a6ace542f445087fdc7dbfcb827d01066561b100
cawleygl <grantlcawley@gmail.com>
2021-02-05 01:40:15 -0600

    Added server.js file to run express server with notes and index routes

---
+++ server.js
@@ -0,0 +1,23 @@
+// Dependencies
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+// Listener
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
