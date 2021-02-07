af46729e3b554b1f7015cb2de9dc3e689c58bd44
cawleygl <grantlcawley@gmail.com>
2021-02-06 22:05:49 -0600

    Added delete route to delete note corresponding with id passed in and re-assign new id's accordingly (to prevent duplicate id's)

--- server.js
+++ server.js
@@ -13,31 +13,45 @@ app.use(express.json());
 
 
 // Routes
-app.get("/notes", function(req, res) {
+app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.get("/api/notes", function(req, res) {
+app.get("/api/notes", function (req, res) {
   res.json(database);
-}); 
+});
 
-app.get("*", function(req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.post("/api/notes", function(req, res) {
+app.post("/api/notes", function (req, res) {
+  // Post new note in database
   const newNote = req.body;
   database.push(newNote);
 
+  // Assign id's to all notes
   for (i of database) {
     i.id = database.indexOf(i) + 1;
   }
+  res.json(newNote);
+});
 
-  console.log(database);
-  res.send(database);
-}); 
+app.delete("/api/notes/:id", function (req, res) {
+// Loop through database and delete note with matching id
+  for (i of database) {
+    if (parseInt(i.id) === parseInt(req.params.id)) {
+      database.splice(req.params.id - 1, 1);
+    }
+  }
+// Re-assign new id's to all notes
+  for (i of database) {
+    i.id = database.indexOf(i) + 1;
+  }
+  res.json(database);
+});
 
 // Listener
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
