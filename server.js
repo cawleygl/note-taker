c5b6d4ef4129716476e7a1461568592ad24b96fd
cawleygl <grantlcawley@gmail.com>
2021-02-10 11:05:04 -0600

    Updated to utilize node filesystem to store data in db.json

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Dependencies
 const express = require("express");
 const path = require("path");
-const database = require("./db/db.json");
+const fs = require("fs");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -11,14 +11,20 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 // Routes
 app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 app.get("/api/notes", function (req, res) {
-  res.json(database);
+  fs.readFile("./db/db.json", 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return
+    }
+    res.send(JSON.parse(data));
+  });
+
 });
 
 app.get("*", function (req, res) {
@@ -26,29 +32,54 @@ app.get("*", function (req, res) {
 });
 
 app.post("/api/notes", function (req, res) {
-  // Post new note in database
   const newNote = req.body;
-  database.push(newNote);
 
-  // Assign id's to all notes
-  for (i of database) {
-    i.id = database.indexOf(i) + 1;
-  }
-  res.json(newNote);
+  // Get data from db.json
+  fs.readFile("./db/db.json", 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return
+    }
+    const database = JSON.parse(data);
+
+    // Assign unique id to new note
+    newNote.id = database.length + 1
+    database.push(newNote);
+
+    // Write to db.json and return newNote
+    fs.writeFile("./db/db.json", JSON.stringify(database), function (err) {
+      if (err) throw err;
+      res.send(newNote);
+    });
+  });
 });
 
 app.delete("/api/notes/:id", function (req, res) {
-// Loop through database and delete note with matching id
-  for (i of database) {
-    if (parseInt(i.id) === parseInt(req.params.id)) {
-      database.splice(req.params.id - 1, 1);
+  // Get data from db.json
+  fs.readFile("./db/db.json", 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return
     }
-  }
-// Re-assign new id's to all notes
-  for (i of database) {
-    i.id = database.indexOf(i) + 1;
-  }
-  res.json(database);
+    const database = JSON.parse(data);
+
+    // Loop through database and delete note with matching id
+    for (i of database) {
+      if (parseInt(i.id) === parseInt(req.params.id)) {
+        database.splice(req.params.id - 1, 1);
+      }
+    }
+    // Re-assign new id's to all notes
+    for (i of database) {
+      i.id = database.indexOf(i) + 1;
+    }
+
+    // Write back to db.json and return data
+    fs.writeFile("./db/db.json", JSON.stringify(database), function (err) {
+      if (err) throw err;
+      res.send(database);
+    });
+  });
 });
 
 // Listener
